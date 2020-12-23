d29bb605c9bf910ec1600654aaa440437c4e3e38
foxlioncode <46830299+foxlioncode@users.noreply.github.com>
2020-12-22 22:56:55 -0500

    JOINED MULTIPLE LINE FUNCTION
    - Joined the alert function into a single immediately invoked function expression.

--- alert.js
+++ alert.js
@@ -1,5 +1,7 @@
 // https://raw.githubusercontent.com/foxlioncode/bookmarklets/master/alert.js
 
-(function(){
-    alert("Hi!!!")
-})();
+// (function(){
+//     alert("Hi!!!")
+// })();
+
+(function(){ alert("Hi!!!") })();
